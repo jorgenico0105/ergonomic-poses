c4ec4d02ab6105d9479f3548979acc760dfee7e5
USUARIO MAC <usuariomac@iMac-de-USUARIO.local>
2025-10-29 16:02:20 -0500

    add port

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,5 @@ config_name = os.getenv('FLASK_ENV', 'production')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
